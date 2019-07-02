63786367d5b54a420bd2461a2b6dd0d68231a7a0
Geevan Patel <geevan.peter.patel@gmail.com>
2019-07-03 00:36:15 +0100

    deleted empty lines in goalCard.js

--- src/goalCard.js
+++ src/goalCard.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import { library } from '@fortawesome/fontawesome-svg-core';
 import './goalCard.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+
 import GoalLikes from './goal-details';
-
-library.add(faThumbsUp);
-
 
 function toTitleCase(str) {
   return str.replace(
@@ -23,12 +18,9 @@ const GoalCard = (props) => (
       <iframe className="video" title={props.goal.etag} width="80" height="65" src={props.URL} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       <span className="title">{toTitleCase(props.goal.snippet.title)}</span>
     <div className="likes-wrapper">
-       
-      
       <GoalLikes
       userLikes={props.goal.statistics.likeCount}
-            />
-      
+      />
     </div>
   </div>
   );
